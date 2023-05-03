99d6adcc795aee8081b3e2d487952114e43464c6
jiminKang <kang@jiminKangs-MacBook-Pro.local>
2023-05-04 01:11:28 +0900

    콜백지옥은 연쇄가 일어나야 함

--- 강지민q1.js
+++ 강지민q1.js
@@ -1,39 +1,37 @@
-function one(value){
-    return value +1;
-}
-function two(value){
-    return value +2;
-}
-function three(value){
-    return value +3;
-}
-function four(value){
-    return value +4;
-}
-function five(value){
-    return value +5;
-}
-
-function q1(number){
-    if(Number.isInteger(number)==true){
-        setTimeout(()=>{
-            one(number)
-            setTimeout(()=>{
-                two(number)
-                setTimeout(()=>{
-                    three(number)
-                    setTimeout(()=>{
-                        four(number)
-                        setTimeout(()=>{
-                            five(number)
-                            console.log(number)
-                        },1000)
-                    },1000)
-                },1000)
-            },1000)
-        },1000)
+function one(value, callback) {
+    callback(value + 1);
+  }
+  
+  function two(value, callback) {
+    callback(value + 2);
+  }
+  
+  function three(value, callback) {
+    callback(value + 3);
+  }
+  
+  function four(value, callback) {
+    callback(value + 4);
+  }
+  
+  function five(value, callback) {
+    callback(value + 5);
+  }
+  
+  function q1(number) {
+    if (Number.isInteger(number)) {
+      one(number, (result1) => {
+        two(result1, (result2) => {
+          three(result2, (result3) => {
+            four(result3, (result4) => {
+              five(result4, (result5) => {
+                console.log(result5);
+              });
+            });
+          });
+        });
+      });
     }
-
-}
-
-console.log(q1(8))
+  }
+  
+  q1(8);
